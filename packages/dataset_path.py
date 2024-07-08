a34a8cb37e0afc5b45dfe3ee97f83883b741eaee
vX <okiror1vinald@gmail.com>
2024-07-08 08:13:36 +0300

    cvs files for the dataset

--- packages/dataset_path.py
+++ packages/dataset_path.py
@@ -17,15 +17,15 @@ ww_test_data_needs_preprocessing = pathlib.Path('Dataset/wake_word/Test_need_pre
 
 
 # File path for sic CSV files
-sic_aug_train_csv_dir = pathlib.Path('files/sic_aug_train.csv')
-sic_train_csv_dir = pathlib.Path('files/sic_train.csv')
-sic_test_csv_dir = pathlib.Path('files/sic_test.csv')
+sic_aug_train_csv_dir = pathlib.Path('Dataset/csv_files/sic_aug_train.csv')
+sic_train_csv_dir = pathlib.Path('Dataset/csv_files/sic_train.csv')
+sic_test_csv_dir = pathlib.Path('Dataset/csv_files/sic_test.csv')
 
 
 # File path for the wake word files
-ww_aug_train_csv_dir = pathlib.Path('files/ww_aug_train.csv')
-ww_train_csv_dir = pathlib.Path('files/ww_train.csv')
-ww_test_csv_dir = pathlib.Path('files/ww_test.csv')
+ww_aug_train_csv_dir = pathlib.Path('Dataset/csv_files/ww_aug_train.csv')
+ww_train_csv_dir = pathlib.Path('Dataset/csv_files/ww_train.csv')
+ww_test_csv_dir = pathlib.Path('Dataset/csv_files/ww_test.csv')
 
 
 # Mfcc json files
